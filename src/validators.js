e40c10a4551cf0d4d7ac0c044ae03968bed8abae
Sokolero <52373058+Sokolero@users.noreply.github.com>
2022-04-29 14:05:18 +0300

    Add validators function

--- src/validators.js
+++ src/validators.js
@@ -1,1 +1,13 @@
-export const required = value => value ? undefined : "required";
+export const required = (value) => (value ? undefined : "required");
+export const isEmail = (value) => /[a-z]+@[a-z]+\.[a-z]+/.test(value);
+export const isValidPassword = (value) => {
+  const regSet = [/[a-z]+/, /[A-Z]+/, /\d+/, /\w{6,}/];
+  regSet.forEach((regexp) => {
+    if (!regexp.test(value)) {
+      return false;
+    }
+  });
+  return true;
+};
+
+export const isMatch = (value, target) => value === target;
